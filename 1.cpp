6132ef85faf394e4200d7d9f8c973283c3f277ee
vardhan79harsh <hustleharsh7906@gmail.com>
2024-07-27 09:51:39 +0530

    Add files via upload

--- 1.cpp
+++ 1.cpp
@@ -1,14 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-int n;
-cin>>n;
-int arr[n];
-int ans=0;
-for(int i=0;i<n;i++){
-    cin>>arr[i];
-    ans+=arr[i];
-}
-ans/=n;
-cout<<ans<<" ";
+string str;
+cin>>str;
+reverse(str.begin(),str.end());
+cout<<str<<endl;
 }
